26bc965a1564ee2fd25468dbacbe4bb330fff283
Atharv Goel <atharvgo@andrew.cmu.edu>
2026-03-11 15:15:51 +0000

    updated covariance cals for dvl

--- src/dvl_bridge.cpp
+++ src/dvl_bridge.cpp
@@ -46,13 +46,23 @@ void DvlBridge::on_step(const Context& ctx) {
     msg.twist.twist.angular.z = 0.0;
 
     msg.twist.covariance.fill(1e6);  // Large default covariance for unmeasured variables
-    const auto lin_vel_cov = diagonal_from_stddev(sf::Vector3(
-        linear_velocity_percent_noise_ * std::abs(lin_vel_x) + linear_velocity_stddev_noise_,
-        linear_velocity_percent_noise_ * std::abs(lin_vel_y) + linear_velocity_stddev_noise_,
-        linear_velocity_percent_noise_ * std::abs(lin_vel_z) + linear_velocity_stddev_noise_));
+
+    const auto proportional_cov = diagonal_from_stddev(sf::Vector3(
+        linear_velocity_percent_noise_ * std::abs(lin_vel_x),
+        linear_velocity_percent_noise_ * std::abs(lin_vel_y),
+        linear_velocity_percent_noise_ * std::abs(lin_vel_z)));
+    const auto absolute_cov = diagonal_from_stddev(sf::Vector3(
+        linear_velocity_stddev_noise_,
+        linear_velocity_stddev_noise_,
+        linear_velocity_stddev_noise_));
+    std::array<double, 9> lin_vel_cov;
+    for (size_t i = 0; i < 9; ++i) {
+        lin_vel_cov[i] = proportional_cov[i] + absolute_cov[i];
+    }
+
     for (size_t r = 0; r < 3; ++r) {
-    std::copy(lin_vel_cov.begin() + r * 3, lin_vel_cov.begin() + r * 3 + 3, msg.twist.covariance.begin() + r * 6);
-}
+        std::copy(lin_vel_cov.begin() + r * 3, lin_vel_cov.begin() + r * 3 + 3, msg.twist.covariance.begin() + r * 6);
+    }
 
     pub_->publish(msg);
 }
